4195788399d80e209adf0535cae9c7cebf2fab63
Andy-K-Taua <>
2025-06-05 16:39:54 +1000

    removed suggested deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,16 +9,9 @@ import authRoutes from "../routes/auth.route.js";
 import subscriptionRoutes from "../routes/subscription.route.js";
 import gamesRoute from '../routes/games.route.js';
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
-
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const frontendBuildPath = path.join(__dirname, '../../../frontend/dist');      
 
 app.use(express.json());
 
@@ -26,19 +19,13 @@ app.use(express.json());
 const PORT = process.env.PORT || 10000; // Default port for Render
 const HOST = '0.0.0.0'; // Bind to 0.0.0.0 for Rende
 
-app.use(express.static(frontendBuildPath));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://gamer-trello.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 
-app.use(express.static(frontendBuildPath));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(frontendBuildPath, 'index.html'));
-  });
 
 app.use("/api/auth", authRoutes)
 app.use("/api/subscriptions", subscriptionRoutes);
