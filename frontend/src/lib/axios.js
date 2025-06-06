6ff3bcdd42858a8ae0524471344ff2d0eea1b05d
Andy-K-Taua <>
2025-06-06 18:24:08 +1000

    installed isMobile

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,17 +1,24 @@
 // frontend/src/lib/axios.js
 
 import axios from 'axios';
+import { isMobile } from 'device-detect';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 console.log('Is development:', isDevelopment);
 
-const baseURL = isDevelopment ? import.meta.env.VITE_API_URL : '/api';
-console.log(baseURL);
+const getBaseURL = () => {
+  const baseURL = isDevelopment 
+    ? import.meta.env.VITE_API_URL 
+    : isMobile 
+      ? `${window.location.protocol}://${window.location.host}/api` 
+      : '/api';
+
+  console.log('current baseURL:', baseURL);
+  return baseURL;
+};
 
 const axiosInstance = axios.create({
-  baseURL,
+  baseURL: getBaseURL(),
 });
-
-console.log("axiosInstance.defaults.baseURL:", axiosInstance.defaults.baseURL);
 
 export { axiosInstance };
